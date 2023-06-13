f4d5ffd8848bdbddee6d64d79ce5d12e4b099bff
Govind P <govind311000@gmail.com>
2023-06-13 18:13:03 +0530

    login

--- client/src/scenes/loginpage/index.jsx
+++ client/src/scenes/loginpage/index.jsx
@@ -1,141 +1,107 @@
-import { Box, Button, TextField,useTheme,Typography,Stack} from "@mui/material";
-import Item from "@mui/material/Stack";
-import { Formik } from "formik";
-import { tokens } from "../../theme";
-import * as yup from "yup";
-import useMediaQuery from "@mui/material/useMediaQuery";
-import Header from "../../components/Header";
-import IconButton from '@mui/material/IconButton';
-import Input from '@mui/material/Input';
-import OutlinedInput from '@mui/material/OutlinedInput';
-import InputLabel from '@mui/material/InputLabel';
-import InputAdornment from '@mui/material/InputAdornment';
-import FormHelperText from '@mui/material/FormHelperText';
-import FormControl from '@mui/material/FormControl';
-import Visibility from '@mui/icons-material/Visibility';
-import VisibilityOff from '@mui/icons-material/VisibilityOff';
 import * as React from 'react';
-  
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
-const LoginPage = () => {
-  const isNonMobile = useMediaQuery("(min-width:600px)");
-  const theme = useTheme();
-  const colors = tokens(theme.palette.mode);
-  const [showPassword, setShowPassword] = React.useState(false);
-  const handleClickShowPassword = () => setShowPassword((show) => !show);
-  const handleMouseDownPassword = (event) => {
+function Copyright(props) {
+  return (
+    <Typography variant="body2" color="text.secondary" align="center" {...props}>
+      {'Copyright © '}
+      <Link color="inherit" href="https://mui.com/">
+        Your Website
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
+
+// TODO remove, this demo shouldn't need to reset the theme.
+
+const defaultTheme = createTheme();
+
+export default function LoginPage() {
+  const handleSubmit = (event) => {
     event.preventDefault();
-  };
-  const handleFormSubmit = (values) => {
-    console.log(values);
+    const data = new FormData(event.currentTarget);
+    console.log({
+      email: data.get('email'),
+      password: data.get('password'),
+    });
   };
 
   return (
-    <Box m="20px">
-
-      <Formik
-        onSubmit={handleFormSubmit}
-        initialValues={initialValues}
-        validationSchema={checkoutSchema}
-      >
-        {({
-          values,
-          errors,
-          touched,
-          handleBlur,
-          handleChange,
-          handleSubmit,
-        }) => (
-        <form onSubmit={handleSubmit}>
-            <Box
-              display="grid"
-              gap="10px"
-              gridTemplateColumns="repeat(4, minmax(0, 1fr))"
-              sx={{
-                "& > div": { gridColumn: isNonMobile ? undefined : "span 3" },
-              }}
-            >
-            <Box
-            component={"span"}
-            backgroundColor={colors.primary[400]}
-            >
-            <Typography
-                variant="h2"
-                fontWeight="600"
-                align="center"
-
-                sx={{ padding: "50px 30px 50px 30px" }}
-            >
-                Login
-            </Typography>
-            <Stack direction="column" spacing={2} >
-            <Item alignItems={"center"}>
-                <TextField sx={{ m: 1, width: '50ch' }}
-                id="user" 
-                variant="outlined"
-                margin="normal"
-                type="text"
-                label="Email"
-                onBlur={handleBlur}
-                onChange={handleChange}
-                value={values.email}
-                name="email"
-                error={!!touched.email && !!errors.email}
-                helperText={touched.email && errors.email}
-                />
-            </Item>
-            <Item alignItems={"center"}>
-            <FormControl sx={{ m: 1, width: '50ch' }} variant="outlined">
-            <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
-            <OutlinedInput
-              id="outlined-adornment-password"
-              type={showPassword ? 'text' : 'password'}
-              endAdornment={
-                <InputAdornment position="end">
-                  <IconButton
-                    aria-label="toggle password visibility"
-                    onClick={handleClickShowPassword}
-                    onMouseDown={handleMouseDownPassword}
-                    edge="end"
-                  >
-                    {showPassword ? <VisibilityOff /> : <Visibility />}
-                  </IconButton>
-                </InputAdornment>
-              }
-              label="Password"
+    <ThemeProvider theme={defaultTheme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign in
+          </Typography>
+          <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+              autoFocus
             />
-            </FormControl>
-            </Item>
-            <Item>
-                <Box display="flex" justifyContent="center" mt="20px">
-                    <Button type="submit" color="secondary" variant="contained">
-                      Login
-                    </Button>
-                </Box>
-            </Item>
-            </Stack>
-            <Box height="250px" mt="-140px" maxWidth={600}>
-            </Box>
-            </Box>
-            </Box>   
-          
-            </form>
-        )}
-      </Formik>
-    </Box>
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Sign In
+            </Button>
+            <Grid container>
+              <Grid item xs>
+                <Link href="/dashboard" variant="body2">
+                  Forgot password?
+                </Link>
+              </Grid>
+              <Grid item>
+                <Link href="#" variant="body2">
+                  {"Don't have an account? Sign Up"}
+                </Link>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+        <Copyright sx={{ mt: 8, mb: 4 }} />
+      </Container>
+    </ThemeProvider>
   );
-};
-
-
-const checkoutSchema = yup.object().shape({
-  email: yup.string().email("invalid email").required("required"),
-  password: yup
-    .string()
-    .required("required"),
-});
-const initialValues = {
-  email: "",
-  password: "",
-};
-
-export default LoginPage;
+}
